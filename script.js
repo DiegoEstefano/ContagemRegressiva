0e6e53829b6fa9b305569c480817088781910143
DiegoEstefano <81423690+DiegoEstefano@users.noreply.github.com>
2021-08-18 15:03:38 -0300

    Alguns bugs resolvidos.
    Desativar botão de iniciar, impedindo de iniciar a contagem várias vezes.
    Agora apenas valores positivos são permitidos.
    Para iniciar novamente uma contagem é preciso pressionar pause.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var contDown;
 const clickON = () => {
 
     clickON.innerHTML = '';
+
     // tempo, em segundos que queremos.
     let seconds = document.getElementById("minutes")
 
@@ -10,43 +11,53 @@ const clickON = () => {
 
     const countDiv = document.getElementById("timer")
 
-    const secpass = () => {
+    // verifica se existe algum valor digitado
+    if (seconds.value <= 0) {
 
-        var min = Math.floor(sec / 60);
+        alert("Digite algum valor maior que zero!")
 
-        var segundosRestantes = sec % 60;
+    } else {
 
-        // Coloca o 0 antes dos segundos EX: 3:03
-        if (segundosRestantes < 10) {
-            segundosRestantes = "0" + segundosRestantes;
-        }
+        const secpass = () => {
 
-        if (min < 10) {
-            min = "0" + min
-        }
+            var min = Math.floor(sec / 60);
 
-        //Vai gerar o formato de 02:59
-        countDiv.innerHTML = `${min}:${segundosRestantes}`
+            var segundosRestantes = sec % 60;
 
-        //Condição final
-        if (sec > 0) {
-            sec -= 1;
-        } else {
-            countDiv.innerHTML = "Acabou!"
-        }
+            // Coloca o 0 antes dos segundos EX: 3:03
+            if (segundosRestantes < 10) {
+                segundosRestantes = "0" + segundosRestantes;
+            }
 
+            if (min < 10) {
+                min = "0" + min
+            }
 
-    };
-    start.style.visibility = "hidden"
-    aviso.style.visibility = "visible"
-    contDown = setInterval(() => secpass(), 1000)
+            //Vai gerar o formato de 02:59
+            countDiv.innerHTML = `${min}:${segundosRestantes}`
 
+            //Condição final
+            if (sec > 0) {
+                sec -= 1;
+            } else {
+                //informa que acabou e reativa o botão e dar a cor verde
+                countDiv.innerHTML = "Acabou!"
+                startButton.style.pointerEvents = "auto"
+                startButton.style.background = "rgb(130, 245, 155)"
+            }
+
+        };
+        startButton.style.background = "grey"
+        startButton.style.pointerEvents = "none"
+
+        contDown = setInterval(() => secpass(), 1000)
+    }
 }
 
 const stopClick = () => {
     clearInterval(contDown)
-    start.style.visibility = "visible"
-    aviso.style.visibility = "hidden"
+    startButton.style.background = "rgb(130, 245, 155)"
+    startButton.style.pointerEvents = "auto"
 }
 const ResetClick = () => {
     let elemento = document.getElementById('timer')
